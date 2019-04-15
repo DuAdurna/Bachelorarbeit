c3835433133fd53d460f672452f95d761033231a
DuAdurna <horstkevin123456789@gmail.com>
2019-04-15 19:59:33 +0200

    Update ErstellungFig82.m

--- Hilfsfunktionen/ErstellungFig82.m
+++ Hilfsfunktionen/ErstellungFig82.m
@@ -1,15 +1,15 @@
 %Hilfe zur erstellung des Sammelgraph benötigt
 clear;Bild6;Netzwerk=Figur61;Netzwerk2=Figur62;Meinungen=MeinungenF61;
 Meinungen=MeinungsRandom(Meinungen);
-Gd = digraph(Netzwerk);
-Gd.Nodes.Meinungen=Meinungen;
-[Gd,plotdataD]=MeinungVerbreitenD(Gd,10,0,0,0.2);
 Gs = digraph(Netzwerk);
 Gs.Nodes.Meinungen=Meinungen;
 [Gs,plotdataS]=MeinungVerbreitenS(Gs,10,0);
-Gd2 = digraph(Netzwerk);
+Gd = digraph(Netzwerk);
+Gd.Nodes.Meinungen=Meinungen;
+[Gd,plotdataD]=MeinungVerbreitenD(Gd,10,0,0,0.2);
+Gd2 = digraph(Netzwerk2);
 Gd2.Nodes.Meinungen=Meinungen;
-[Gd2,plotdataD2]=MeinungVerbreitenD(Gd,10,0,0,0.2);
+[Gd2,plotdataD2]=MeinungVerbreitenD(Gd2,10,0,0,0.2);
 %% hier werden dann die Plots erstellt
 semilogy(plotdataD2(1,:),plotdataD2(3,:),'-b');
 hold on
@@ -18,5 +18,3 @@ hold on
 semilogy(plotdataS(1,:),plotdataS(3,:),'-.g');
 legend('Lambda2=.48(deterministisch)', 'Lambda2=.13 (deterministisch)', 'Lambda2=.13(stochastisch)')
 hold off
-
-
